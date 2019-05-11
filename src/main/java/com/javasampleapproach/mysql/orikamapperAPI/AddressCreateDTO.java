9a234e54e6b80a39a35770a805a29f292ecb89e1
nikolacloud12 <stanojenikola@gmail.com>
2019-05-11 20:19:22 +0200

    last comint

---
+++ src/main/java/com/javasampleapproach/mysql/orikamapperAPI/AddressCreateDTO.java
@@ -0,0 +1,49 @@
+package com.javasampleapproach.mysql.dto.address;
+
+import javax.validation.constraints.NotNull;
+
+public class AddressCreateDTO {
+	
+	private Long addressId;
+	@NotNull
+	private String addressName;
+
+	@NotNull
+	private String county;
+	@NotNull
+	private String street;
+
+
+
+	public Long getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(Long addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getAddressName() {
+		return addressName;
+	}
+
+	public void setAddressName(String addressName) {
+		this.addressName = addressName;
+	}
+
+	public String getCounty() {
+		return county;
+	}
+
+	public void setCounty(String county) {
+		this.county = county;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+}
